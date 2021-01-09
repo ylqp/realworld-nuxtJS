9da7820e587fcacef699c62a3304b0793ac08423
Xiaofang  Niu <xiaofangniu@XiaofangdeMacBook-Pro.local>
2021-01-09 22:20:51 +0800

    init

---
+++ nuxt.config.js
@@ -0,0 +1,44 @@
+/**
+ * NuxtJS配置文件
+ */
+
+ module.exports = {
+     router: {
+        extendRoutes (routes, resolve) {
+            console.log(routes)
+            // 清空默认生成路由
+            routes.splice(0)
+            // 自己设置路由
+            routes.push(...[
+                {
+                    path: '/',
+                    component: resolve(__dirname, 'pages/layout/'),
+                    children: [
+                        {
+                            path: '',
+                            name: 'home',
+                            component: resolve(__dirname, 'pages/home/')
+
+                        },
+                        {
+                            path: '/login',
+                            name: 'login',
+                            component: resolve(__dirname, 'pages/login/')
+                        },
+                        {
+                            path: '/register',
+                            name: 'register',
+                            component: resolve(__dirname, 'pages/login/')
+                        },
+                        {
+                            path: '/profile/:username?',
+                            name: 'profile',
+                            component: resolve(__dirname, 'pages/profile/')
+                        },
+                    ]
+                }
+            ])
+
+        }
+     }
+ }
